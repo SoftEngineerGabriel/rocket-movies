cfeb56eff17e07c3a8aecdbf85dc29befe729034
Gabriel Farias <farias2752@gmail.com>
2023-11-16 22:03:52 -0300

    Cabeçalho do home

--- rocket-movies/src/pages/Home/index.jsx
+++ rocket-movies/src/pages/Home/index.jsx
@@ -1,12 +1,26 @@
 import { Container } from './styles';
 
+import { FiPlus } from 'react-icons/fi'
+
+
 import {Header} from '../../components/Header'
+import {Button} from '../../components/Button'
 
 export function Home() {
     return(
         <Container>
             <Header/>
+            
+            <main>
+                <div>
+                    <h1>Meus filmes</h1>
+                    <Button 
+                    title="Adicionar filme"
+                    icon={FiPlus}
+                    />
 
+                </div>
+            </main>
         </Container>
     )
 }
--- rocket-movies/src/pages/Home/styles.js
+++ rocket-movies/src/pages/Home/styles.js
@@ -1,5 +1,33 @@
 import styled from "styled-components";
 
+import plusButton from '../../assets/plusButton.svg'
+
 export const Container = styled.div`
-    
+    > main {
+        margin: 50px 123px;
+
+        div {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            > Button {
+                width: 207px;
+
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                justify-items: center;
+                text-align: center;
+                
+                color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+                font-feature-settings: 'liga' off;
+                font-family: Roboto Slab;
+                font-size: 16px;
+                font-style: normal;
+                font-weight: 400;
+                line-height: normal;
+            }
+        }
+    }
 `;
